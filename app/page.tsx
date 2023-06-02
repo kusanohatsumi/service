cf16f45e77da99297f9a92ccc3a9ddd875621ef9
kusano <kusano.hatsumi@gmail.com>
2023-06-02 15:50:45 +0900

    タイトルとテキストを追加

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
-import Image from 'next/image'
+import Image from 'next/image';
 
 export default function Home() {
   return (
-    <><main><h1>あああ</h1></main></>
-  )
+    <>
+      <main>
+        <h1>新規登録</h1>
+        <p>名前</p>
+      </main>
+    </>
+  );
 }
