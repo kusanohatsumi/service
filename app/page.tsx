974db2c60d32629f9ca1d7e7f112a840481a4b5e
kusano <kusano.hatsumi@gmail.com>
2023-06-06 13:57:57 +0900

    disable:CSS,app/page.tsx
    globalCSSの内容を削除

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,7 @@
-import Image from 'next/image';
-
 export default function Home() {
   return (
     <>
-      <main>
-        <h1>新規登録</h1>
-        <p>名前</p>
-      </main>
+      <main></main>
     </>
   );
 }
