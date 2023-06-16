9433aef323d9f419535353c4b493802e92ecafd0
Sora0926 <2210126@ecc.ac.jp>
2023-06-16 13:39:52 +0900

    add:cssの追加
    草野さんのbuttonを追加したもの

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,3 +1,35 @@
+'use client';
+import Button__border from '@/ui/button/button-border';
+import border_buttom from '@/ui/button/border-buttom';
+import Button from '@/ui/button/button';
+import styled from 'styled-components';
+import FlexCenter from '@/ui/center';
+
+
+const Container = styled.div`
+  position: relative;
+  width: 100vw;
+  height: 100vh;
+  padding-top: 15vh;
+  @media (max-height: 700px) {
+    padding-top: 4vh;
+  }
+`;
+
+const Button_bg = styled.div`
+  width: 162px;
+  height: 42px;
+  background: linear-gradient(
+    90deg,
+    rgba(244, 188, 247, 1) 0%,
+    rgba(154, 229, 225, 1) 100%
+  );
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-radius: 25px;
+`;
+
 export default function Login() {
   return (
     <>
