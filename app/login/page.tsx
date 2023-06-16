e80c7f8ef82f2d760018df81ab94f656464553ee
Sora0926 <2210126@ecc.ac.jp>
2023-06-16 13:38:47 +0900

    add:test中
    特に大事な変更なし

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,7 +1,20 @@
 export default function Login() {
   return (
     <>
-      <h1>ログイン</h1>
+      <h2>ユーザーネーム</h2>
+      <h2>ID</h2>
+
+      <Container>
+        <div className="flex flex-col items-center">          
+          {/* ボタン */}
+          <FlexCenter className=" flex-col">
+            <Button className="mb-4">新規登録</Button>
+            <Button_bg>
+              <Button__border>ログイン</Button__border>
+            </Button_bg>
+          </FlexCenter>
+        </div>
+      </Container>
     </>
   );
 }
---
+++ ui/button/border-buttom.tsx
@@ -0,0 +1,11 @@
+// ボタン
+'use client';
+
+import styled from 'styled-components';
+
+const border_buttom = styled.button`
+    background: #fff;
+    border-bottom: solid 1px #ccc;
+`;
+
+export default border_buttom;
