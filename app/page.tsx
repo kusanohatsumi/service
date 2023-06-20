9fac816751fd9696b7becce509df48a89fdd2880
HatsumiKusano <kusano.hatsumi@gmail.com>
2023-06-20 10:09:13 +0900

    update:初期化

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,7 @@
-'use client';
-
-import Navlink from '@/ui/nav_link';
-import Head from '@/ui/nav_head';
-
 export default function Home() {
   return (
     <>
-      <main>
-        <Head />
-        <Navlink />
-      </main>
+      <main></main>
     </>
   );
 }
