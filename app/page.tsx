b87194cf499579ebe92d30bf9f8e0e5ee340dadc
HatsumiKusano <kusano.hatsumi@gmail.com>
2023-06-20 10:03:38 +0900

    update:不要なファイル削除

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,7 @@
-import Form from '@/ui/form';
-import Registration from './registration/page';
-
 export default function Home() {
   return (
     <>
-      <main>
-        <Form />
-      </main>
+      <main></main>
     </>
   );
 }
