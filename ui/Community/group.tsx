371ac9290bf1e4ed520d80a1b6910c596a616b61
Sora0926 <2210126@ecc.ac.jp>
2023-06-23 15:32:48 +0900

    Update group.tsx

--- ui/Community/group.tsx
+++ ui/Community/group.tsx
@@ -3,24 +3,33 @@
 import styled from 'styled-components';
 import Image from 'next/image';
 
-// const Group = styled.div`
-//   <input type="text" name="name" value="" placeholder="山田 太郎">
-// `;
+const Transparent = styled.input.attrs({
+  type: 'text',
+  name: 'name',
+  placeholder: 'グループを検索',
+  
+})`outline:none;
+  
+`;
 
 export default function Group() {
   return (
     <>
 
       <h1>グループ</h1>
-      <div className=''>
-      <Image
-        src="/search.svg"
-        alt="magn glassifying"
-        width={24}
-        height={24}
-      />
-      <input type="text" name="name" value="" placeholder="グループを検索"></input>
+
+      <div id='pink' className='flex items-center w-64 border-2 bordre-solid border-vercel-pink'>
+
+        <Image
+          src="/search.svg"
+          alt="magn glassifying"
+          width={24}
+          height={24}
+        />
+
+        <Transparent></Transparent>
       </div>
+
     </>
   );
 }
