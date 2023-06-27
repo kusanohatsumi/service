0aa54c5ced794a794daf40383a0529f3837645db
HatsumiKusano <kusano.hatsumi@gmail.com>
2023-06-27 09:38:21 +0900

    update:QuestionChoiceBtnからcheckboxを削除
    checkboxを別ファイルに移設したので不要な部分を削除

--- ui/question/question_choicebtn.tsx
+++ ui/question/question_choicebtn.tsx
@@ -1,53 +1,6 @@
 'use client';
 import styled from 'styled-components';
-
-// チェクボックス
-const Input = styled.input.attrs({
-  type: 'radio',
-  name: 'choice',
-})`
-  cursor: pointer;
-  width: 158px;
-  height: 38px;
-  appearance: none;
-  position: relative;
-  top: 0;
-  left: 0;
-  z-index: 2;
-  &::before,
-  &::after {
-    content: '';
-    width: 22px;
-    height: 22px;
-    display: block;
-    position: absolute;
-    top: 8.5px;
-    left: 12px;
-
-  }
-  &::before {
-    border: dashed 2px #8ad8d4;
-    border-radius: 50%;
-    background: #fff;
-  }
-  &::after {
-    width: 22px;
-    height: 12px;
-    opacity: 0;
-    top:9px;
-    left:0px
-    outline:solid 2px skyblue;
-    transform: rotate(-45deg);
-    border-left: solid 3px #8ad8d4;
-    border-bottom: solid 3px #8ad8d4;
-  }
-  &:checked::before {
-    border: none;
-  }
-  &:checked::after {
-    opacity: 1;
-  }
-`;
+import Checkbox from './question_button';
 
 const Button__border = styled.div`
   background: #fff;
@@ -99,12 +52,12 @@ export default function QuestionChoiceBtn() {
     <>
       <form>
         <Button className="checkbox">
-          <Input defaultChecked />
+          <Checkbox defaultChecked />
           <Button__border></Button__border>
         </Button>
 
         <Button className="checkbox">
-          <Input />
+          <Checkbox />
           <Button__border></Button__border>
         </Button>
       </form>
